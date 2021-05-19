19dba7a42c26e8287eaecb128c7aca8834d7127d
Charlie Stokes <chstokes7@gmail.com>
2021-05-19 16:11:49 -0500

    updated database.sql

--- database.sql
+++ database.sql
@@ -16,4 +16,11 @@ CREATE TABLE "item" (
     "user_id" INT REFERENCES "user"
 );
 
+INSERT INTO item (description, image_url, user_id) VALUES 
+('Fly You Fools', 'https://i.imgur.com/SM96caR.gif', 1),
+('Rockin', 'https://i.imgur.com/0tykmxD.gif', 1),
+('Too Long...', 'https://i.imgur.com/EdPLU67.gif', 1),
+('GD APPLE', 'https://www.markon.com/sites/default/files/styles/large/public/pi_photos/Apples_Honeycrisp_Hero.jpg', 2);
+
+
 -- comments
