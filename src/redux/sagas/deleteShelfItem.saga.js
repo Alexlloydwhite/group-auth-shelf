8d961697ea11ebc8562a6e40e635efe92a28c21e
Alex White <alexlloydwhite@gmail.com>
2021-05-21 09:15:52 -0500

    state refreshes on page change correctly

--- src/redux/sagas/deleteShelfItem.saga.js
+++ src/redux/sagas/deleteShelfItem.saga.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 function* deleteShelfItem(action) {
     try {
         yield axios.delete(`/api/shelf/${action.payload}`);
-        yield put({ type: 'FETCH_ITEMS' });
+        yield put({ type: 'FETCH__MY_ITEMS' });
     } catch (error) {
         // alert(`Sorry things aren't working at the moment. Try again later.`);
         console.log('Error deleting item', error);
